546799ad4dde73945b2f147d958d26c5febfb719
Miguel Guimaraes <miguel.guimaraes053722@gmail.com>
2025-07-02 16:52:13 +0100

    fix: Remove Vuetify, Added tailwindCss and new UI in login page

--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
-import { createVuetify } from 'vuetify'
 import router from './router.js'
-import 'vuetify/styles' // Global CSS has to be imported
-import { aliases, mdi } from 'vuetify/iconsets/mdi' // Optional icons
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import './assets/tailwind.css'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: { mdi },
-  },
-})
 const app = createApp(App)
-app.use(vuetify)
 app.use(router)
 app.use(store)
 
--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,22 +1,47 @@
 <template>
-  <v-app>
-    <v-sheet class="mx-auto mt-4" width="400">
-      <h1 class="text-center">Gym Tracker App</h1>
-      <v-form @submit.prevent="submitForm">
-        <v-text-field v-model.trim="username" label="Username"></v-text-field>
-        <v-text-field
-          v-model.trim="password"
+  <div class="h-screen flex items-center justify-center">
+    <form class="max-w-sm" @submit.prevent="submitForm">
+      <h1 class="mb-20">Gym Weigth Tracker</h1>
+      <div class="mb-10">
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900"
+          >Your username</label
+        >
+        <input
+          type="email"
+          id="email"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="name@flowbite.com"
+          required
+          v-model.trim="username"
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900"
+          >Your password</label
+        >
+        <input
           type="password"
-          label="Password"
-        ></v-text-field>
-        <v-btn class="mt-2" type="submit" block>Submit</v-btn>
-        <p v-if="!formIsValid">
-          Please enter a valid email and password (must be at least 6 characters
-          long)
-        </p>
-      </v-form>
-    </v-sheet>
-  </v-app>
+          id="password"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          required
+          v-model.trim="password"
+        />
+      </div>
+      <button
+        type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </form>
+
+    <p v-if="!formIsValid">
+      Please enter a valid email and password (must be at least 6 characters
+      long)
+    </p>
+  </div>
 </template>
 
 <script setup>
